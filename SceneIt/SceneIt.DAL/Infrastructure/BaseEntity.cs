aca832396092ecdfa49c7a72a0291d2a4b4b7b10
Cory Caswell <Corneleus1@gmail.com>
2020-01-02 22:48:23 -0500

    made a base entity

--- SceneIt/SceneIt.DAL/Infrastructure/BaseEntity.cs
+++ SceneIt/SceneIt.DAL/Infrastructure/BaseEntity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SceneIt.DAL.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace SceneIt.DAL.Infrastructure
 {
-    class BaseEntity
+    public class BaseEntity : IEntity
     {
+
     }
 }
