f45c1a32ef7d06aa6e8e797e8e5826850d505124
Cory Caswell <Corneleus1@gmail.com>
2020-01-19 18:52:00 -0500

    finished DAL and added service

---
+++ SceneIt/SceneIt.API/Controllers/MediaController.cs
@@ -0,0 +1,58 @@
+﻿using SceneIt.API.Interfaces;
+using SceneIt.API.Services;
+using SceneIt.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace SceneIt.API.Controllers
+{
+    [RoutePrefix("api/media")]
+    public class MediaController : ApiController
+    {
+        private readonly IMediaService mediaService;
+
+        public MediaController()
+        {
+            this.mediaService = new MediaService();
+        }
+        
+        [Route("")]
+        [HttpGet]
+        public List<Media> GetAll()
+        {
+            try
+            {
+                return this.mediaService.GetAll();
+            }
+            catch (Exception)
+            {
+                return new List<Media>();
+                
+            }           
+        }
+
+        [Route("/(id:int)")]
+        [HttpGet]
+        public Media Get(int id)
+        {
+            try
+            {
+                return this.mediaService.Get(id);
+            }
+            catch (Exception)
+            {
+                return default(Media);
+            }
+            
+        }
+                
+        // POST api/<controller>
+        //public void Post([FromBody]string value)
+        //{
+        //}
+    }
+}
---
+++ SceneIt/SceneIt.API/Interfaces/IMediaService.cs
@@ -0,0 +1,16 @@
+﻿using SceneIt.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SceneIt.API.Interfaces
+{
+    public interface IMediaService
+    {
+        List<Media> GetAll();
+        Media Get(int id);
+       
+    }
+}
