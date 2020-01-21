f75efb5cfca0ab6e65e593ec1b7ccbbf4f98221e
Cory Caswell <Corneleus1@gmail.com>
2020-01-21 13:33:32 -0500

    Added service, controller and interface for user and user media

---
+++ SceneIt/SceneIt.API/Controllers/UserMediaController.cs
@@ -0,0 +1,52 @@
+﻿using SceneIt.API.Interfaces;
+using SceneIt.API.Services;
+using SceneIt.DAL.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+namespace SceneIt.API.Controllers
+{
+    [RoutePrefix("api/usermedia")]
+    public class UserMediaController : ApiController
+    {
+        private readonly IUserMediaService userMediaService;
+
+        public UserMediaController()
+        {
+            this.userMediaService = new UserMediaService();
+        }
+
+        [Route("")]
+        [HttpGet]
+        public List<UserMedia> GetAll()
+        {
+            try
+            {
+                return this.userMediaService.GetAll();
+            }
+            catch (Exception)
+            {
+
+                return new List<UserMedia>();
+            }
+        }
+
+        [Route("/(id:int)")]
+        [HttpGet]
+        public UserMedia Get(int id)
+        {
+            try
+            {
+                return this.userMediaService.Get(id);
+            }
+            catch (Exception)
+            {
+                return default(UserMedia);
+            }
+
+        }
+    }
+}
