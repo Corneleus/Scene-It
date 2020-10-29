fd128cd2831f065a54af1acd8392644a07d174bd
Corneleus <CaswellCA@gmail.com>
2020-10-29 11:39:49 -0400

    added syle to target specific child div so that there are columns in the genre tags drop down

--- Scene It UI/SceneIt/src/app/Component/comics/add-comic/add-comic.component.ts
+++ Scene It UI/SceneIt/src/app/Component/comics/add-comic/add-comic.component.ts
@@ -14,60 +14,55 @@ export class AddComicComponent implements OnInit {
   closeResult: string = '';
   comics: Comic[] = [];
   title: string;
-  
+
   items = [
-    {id: 1,  name: 'Action'},
-    {id: 2,  name: 'Adventure'},
-    {id: 3,  name: 'Comedy'},
-    {id: 4,  name: 'Drama'},
-    {id: 5,  name: 'Slice of Life'},
-    {id: 6,  name: 'Fantasy'},
-    {id: 7,  name: 'Magic'},
-    {id: 8,  name: 'Supernatural'},
-    {id: 9,  name: 'Horror'},
-    {id: 10, name: 'Mystery'},
-    {id: 11, name: 'Psycological'},
-    {id: 12, name: 'Romance'},
-    {id: 13, name: 'Sci-Fi'},
-    {id: 14, name: 'Magic'},
-    {id: 15, name: 'Supernatural'},
-    {id: 16, name: 'Cyberpunk'},
-    {id: 17, name: 'Game'},
-    {id: 18, name: 'Ecchi'},
-    {id: 19, name: 'Demons'},
-    {id: 20, name: 'Harem'},
-    {id: 21, name: 'Josei'},
-    {id: 22, name: 'Martial Arts'},
-    {id: 23, name: 'Kids'},
-    {id: 24, name: 'Historial'},
-    {id: 25, name: 'Hentai'},
-    {id: 26, name: 'Isekai'},
-    {id: 27, name: 'Military'},
-    {id: 28, name: 'Mecha'},
-    {id: 29, name: 'Music'},
-    {id: 30, name: 'Parody'},
-    {id: 31, name: 'Police'},
-    {id: 32, name: 'Post-Apocalyptic'},
-    {id: 33, name: 'Reverse Harem'},
-    {id: 34, name: 'School'},
-    {id: 35, name: 'Seinen'},
-    {id: 36, name: 'Shounen'},
-    {id: 37, name: 'Shoujo'},
-    {id: 38, name: 'Shoujo-ai'},
-    {id: 39, name: 'Space'},
-    {id: 40, name: 'Sports'},
-    {id: 41, name: 'Super Powers'},
-    {id: 42, name: 'Tragedy'},
-    {id: 43, name: 'Vampire'},
-    {id: 44, name: 'Yuri'},
-    {id: 45, name: 'Yaoi'},
- 
+    { id: 1, name: 'Action' },
+    { id: 2, name: 'Adventure' },
+    { id: 3, name: 'Comedy' },
+    { id: 4, name: 'Drama' },
+    { id: 5, name: 'Slice of Life' },
+    { id: 6, name: 'Fantasy' },
+    { id: 7, name: 'Magic' },
+    { id: 8, name: 'Supernatural' },
+    { id: 9, name: 'Horror' },
+    { id: 10, name: 'Mystery' },
+    { id: 11, name: 'Psycological' },
+    { id: 12, name: 'Romance' },
+    { id: 13, name: 'Sci-Fi' },
+    { id: 14, name: 'Cyberpunk' },
+    { id: 15, name: 'Game' },
+    { id: 16, name: 'Ecchi' },
+    { id: 17, name: 'Demons' },
+    { id: 18, name: 'Harem' },
+    { id: 19, name: 'Josei' },
+    { id: 20, name: 'Martial Arts' },
+    { id: 21, name: 'Kids' },
+    { id: 22, name: 'Historial' },
+    { id: 23, name: 'Hentai' },
+    { id: 24, name: 'Isekai' },
+    { id: 25, name: 'Military' },
+    { id: 26, name: 'Mecha' },
+    { id: 27, name: 'Music' },
+    { id: 28, name: 'Parody' },
+    { id: 29, name: 'Police' },
+    { id: 30, name: 'Post-Apocalyptic' },
+    { id: 31, name: 'Reverse Harem' },
+    { id: 32, name: 'School' },
+    { id: 33, name: 'Seinen' },
+    { id: 34, name: 'Shounen' },
+    { id: 35, name: 'Shoujo' },
+    { id: 36, name: 'Shoujo-ai' },
+    { id: 37, name: 'Space' },
+    { id: 38, name: 'Sports' },
+    { id: 39, name: 'Super Powers' },
+    { id: 40, name: 'Tragedy' },
+    { id: 41, name: 'Vampire' },
+    { id: 42, name: 'Yuri' },
+    { id: 43, name: 'Yaoi' },
+
 
   ];
-  selected = [
-
-  ];
-
+  selected = [];
 
   constructor(private modalService: NgbModal) { }
 
@@ -94,8 +89,6 @@ export class AddComicComponent implements OnInit {
       return `with: ${reason}`;
     }
   }
-
-
 }
 
 
